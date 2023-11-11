3fb63da417e9ad02fba48b690fe6970781bbf35b
amir944 <seyyediamir944@gmail.com>
2023-11-11 16:38:03 +0330

    ubdate

--- src/data.js
+++ src/data.js
@@ -2,29 +2,39 @@
 const swiperWrapperbestPortfolio = document.querySelector("#bestPortfolio");
 data = [
   {
+    image: "../assets/star/img1.png",
+    title: "طراحی رابط کاربری برای سیستم عامل مک",
+    rating: "متوسط",
+    score: "../assets/S5.png",
+    category: "design",
+  },
+  {
     image: "../assets/star/img8.png",
     title: "مدیریت پروژه منابع انسانی",
+    rating: "متوسط",
     score: "../assets/s5.png",
+    category: "programming",
   },
   {
     image: "../assets/star/img7.png",
     title: "طراحی چارت ها و گزارش های ماهانه شرکت بهسان",
+    rating: "متوسط",
     score: "../assets/s5.png",
+    category: "programming",
   },
   {
     image: "../assets/star/img6.png",
     title: "ساخت اپ ورزشی برای لاغری و عضله سازی",
+    rating: "متوسط",
     score: "../assets/s5.png",
+    category: "programming",
   },
   {
     image: "../assets/star/img3.png",
     title: "برنامه نویسی پروژه های کاربری نسخه موبایل...",
+    rating: "متوسط",
     score: "../assets/s5.png",
-  },
-  {
-    image: "../assets/star/img7.png",
-    title: "طراحی چارت ها و گزارش های ماهانه شرکت بهسان",
-    score: "../assets/s5.png",
+    category: "programming",
   },
 ];
 
@@ -35,10 +45,14 @@ data.forEach((item, key) => {
 
     const slideContent = `
     <div class="bg-[#fafafa] dark:bg-[#28293d] dark:text-white w-[290px] lg:w-[280px] h-[340px] p-2 m-2 mb-2 rounded-xl">
-    <img src="${item.image}" class="rounded-xl" alt="" />
-    <p class="font-[dana-light] text-[13px] text-center pt-4">
+    <a 
+    href="./portfolio/portfolioDetails.html"
+    ><img src="${item.image}" class="rounded-xl" alt="" /></a>
+    <a 
+    href="./portfolio/portfolioDetails.html"
+    ><p class="font-[dana-light] text-[13px] text-center pt-4">
       ${item.title}
-    </p>
+    </p></a>
     <div class="flex items-center mr-2 mt-2">
       <img src="../assets/v6-icon (free)4.png" class="pl-2" alt="" />
       <p class="text-[gray]">${item.rating}</p>
@@ -154,25 +168,29 @@ function portfolioDataSet(category = "all") {
       swiperSlide.className = "swiper-slide";
 
       const slideContent = `
-  <div class="bg-[#fafafa] dark:bg-[#28293d] dark:text-white w-[290px] lg:w-[280px] h-[340px] p-2 m-2 mb-2 rounded-xl">
-  <img src="${item.image}" class="rounded-xl" alt="" />
-  <p class="font-[dana-light] text-[13px] text-center pt-4">
-    ${item.title}
-  </p>
-  <div class="flex items-center mr-2 mt-2">
-    <img src="../assets/v6-icon (free)4.png" class="pl-2" alt="" />
-    <p class="text-[gray]">${item.rating}</p>
-  </div>
-  <div class="flex items-center justify-between py-6">
-    <img src="${item.score}" alt="" />
-    <a 
+      <div class="bg-[#fafafa] dark:bg-[#28293d] dark:text-white w-[290px] lg:w-[280px] h-[340px] p-2 m-2 mb-2 rounded-xl">
+      <a 
       href="./portfolio/portfolioDetails.html"
-      class="flex border-[none] items-center bg-black text-white p-2 rounded-xl"
-    >
-      <img src="../assets/v6-icon (free)2.png" alt="" />
-    </a>
-  </div>
-</div>
+      ><img src="${item.image}" class="rounded-xl" alt="" /></a>
+      <a 
+      href="./portfolio/portfolioDetails.html"
+      ><p class="font-[dana-light] text-[13px] text-center pt-4">
+        ${item.title}
+      </p></a>
+      <div class="flex items-center mr-2 mt-2">
+        <img src="../assets/v6-icon (free)4.png" class="pl-2" alt="" />
+        <p class="text-[gray]">${item.rating}</p>
+      </div>
+      <div class="flex items-center justify-between py-6">
+        <img src="${item.score}" alt="" />
+        <a 
+          href="./portfolio/portfolioDetails.html"
+          class="flex border-[none] items-center bg-black text-white p-2 rounded-xl"
+        >
+          <img src="../assets/v6-icon (free)2.png" alt="" />
+        </a>
+      </div>
+    </div>
 `;
 
       swiperSlide.innerHTML = slideContent;
